c5aa710ded9272e3d9dafa78f73b46d3e9644f07
lellis09 <lishia.ellis09@gmail.com>
2024-01-15 17:56:31 -0500

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        myTeal: '#00C4D4',
+        lightgreen: '#E7F740',
+        myindigo: '#4F56F6',
+        hotpink: '#F11AA8',
+        mywhite: '#FAFAFA',
+        navy: '#0D052E',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
