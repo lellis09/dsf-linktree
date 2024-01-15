83a6bab91ff52386915a904a5f67a43d3a89ddce
lellis09 <lishia.ellis09@gmail.com>
2024-01-15 18:22:45 -0500

    made changes to responsiveness

--- app/page.js
+++ app/page.js
@@ -4,10 +4,10 @@ import profilePic from '../public/20210327_150727(1).jpg'
 
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center bg-gradient-to-b from-myindigo to bg-navy">
+    <main className="flex h-screen flex-col items-center bg-gradient-to-b from-myindigo to bg-navy  px-12">
       {/* <div className='bg-lightgreen h-40 w-full'>
       </div> */}
-      <section className='flex flex-col border-[1.75px] border-lightgreen items-center mt-16 pt-12 w-full max-w-[42rem] lg:w-1/2'>
+      <section className='flex flex-col  items-center mt-16 pt-12 w-full max-w-[42rem] lg:w-1/2'>
         <div className='rounded-full bg-teal-500 w-20 h-20 lg:w-44 lg:h-44 border-2 border-lightgreen overflow-hidden relative'>
           <Image 
             src={profilePic}
@@ -23,7 +23,7 @@ export default function Home() {
           <p className='text-white my-8'>Making your online presence awesome</p>
           <div className='flex flex-col'>
             <a href='https://twitter.com/TealTernary'target='_blank' rel='noreferrer'>
-            <button className='rounded-2xl border-[1.75px] border-lightgreen w-[24rem] lg:w-[42rem] h-12 flex justify-center items-center'type='button' >
+            <button className='rounded-2xl bg-lightgreen w-[24rem] lg:w-[42rem] h-12 flex justify-center items-center'type='button' >
               <p className='text-white'>Twitter</p> 
               </button>
            </a>
