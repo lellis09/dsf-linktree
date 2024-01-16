291aace4fbcba76d176016886d66a26117df2b62
lellis09 <lishia.ellis09@gmail.com>
2024-01-15 19:03:21 -0500

    made changes to responsiveness

--- app/page.js
+++ app/page.js
@@ -4,11 +4,11 @@ import profilePic from '../public/20210327_150727(1).jpg'
 
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center bg-gradient-to-b from-myindigo to bg-navy  px-12">
+    <main className="flex h-screen flex-col items-center bg-gradient-to-b from-myTeal to bg-myindigo2  px-12">
       {/* <div className='bg-lightgreen h-40 w-full'>
       </div> */}
       <section className='flex flex-col  items-center mt-16 pt-12 w-full max-w-[42rem] lg:w-1/2'>
-        <div className='rounded-full bg-teal-500 w-20 h-20 lg:w-44 lg:h-44 border-2 border-lightgreen overflow-hidden relative'>
+        <div className='rounded-full bg-teal-500 w-24 h-24 lg:w-48 lg:h-48 border-4 border-lightgreen overflow-hidden relative'>
           <Image 
             src={profilePic}
             alt= "picture of the owner of page"
@@ -19,12 +19,27 @@ export default function Home() {
             }}    
           />
         </div>
-          <h1 className='text-white mt-4 text-4xl'>Lishia Ellis</h1>
-          <p className='text-white my-8'>Making your online presence awesome</p>
-          <div className='flex flex-col'>
+          <h1 className='text-mywhite mt-4 text-5xl'>Lishia Ellis</h1>
+          <p className='text-mywhite text-xl mt-4 mb-12'>Making your online presence awesome</p>
+          <div className='flex flex-col gap-8'>
             <a href='https://twitter.com/TealTernary'target='_blank' rel='noreferrer'>
-            <button className='rounded-2xl bg-lightgreen w-[24rem] lg:w-[42rem] h-12 flex justify-center items-center'type='button' >
-              <p className='text-white'>Twitter</p> 
+            <button className='rounded-2xl bg-gradient-to-r from lightgreen to-myTeal w-[24rem] lg:w-[42rem] h-12 flex justify-center items-center hover:bg-lightgreen'type='button' >
+              <p className='text-navy text-xl uppercase'>Twitter</p> 
+              </button>
+           </a>
+           <a href='https://github.com/lellis09'target='_blank' rel='noreferrer'>
+            <button className='rounded-2xl bg-gradient-to-r from lightgreen to-myTeal w-[24rem] lg:w-[42rem] h-12 flex justify-center items-center hover:bg-lightgreen'type='button' >
+              <p className='text-navy text-xl uppercase'>Github</p> 
+              </button>
+           </a>
+           {/* <a href='https://twitter.com/TealTernary'target='_blank' rel='noreferrer'>
+            <button className='rounded-2xl bg-gradient-to-r from lightgreen to-myTeal w-[24rem] lg:w-[42rem] h-12 flex justify-center items-center hover:bg-lightgreen'type='button' >
+              <p className='text-navy text-xl uppercase'>Work With Me</p> 
+              </button>
+           </a> */}
+          <a href='https://www.linkedin.com/in/lishia-ellis/'target='_blank' rel='noreferrer'>
+            <button className='rounded-2xl bg-gradient-to-r from lightgreen to-myTeal w-[24rem] lg:w-[42rem] h-12 flex justify-center items-center hover:bg-lightgreen'type='button' >
+              <p className='text-navy text-xl uppercase'>Linkedin</p> 
               </button>
            </a>
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,14 @@ module.exports = {
         myTeal: '#00C4D4',
         lightgreen: '#E7F740',
         myindigo: '#4F56F6',
+        myindigo2: '#4045C3',
         hotpink: '#F11AA8',
         mywhite: '#FAFAFA',
         navy: '#0D052E',
+        gradient:'linear-gradient(to right, #4F56F6, #F11AA)',
       },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-linear': 'linear-gradient(to right, #4F56F6, #F11AA)',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
