d94a2648f41edeb28d69118ff79518ab4a46daa9
unknown <lishia.ellis09@gmail.com>
2024-03-10 22:50:25 -0400

    made style changes

--- app/page.js
+++ app/page.js
@@ -23,12 +23,12 @@ export default function Home() {
           <p className='text-mywhite text-lg mt-4 mb-10'>Web Design | Web Development | UX/UI</p>
           <div className='flex flex-col gap-5'>
             <a href='https://twitter.com/TealTernary'target='_blank' rel='noreferrer'>
-            <button className='text-navy text-lg uppercase rounded-2xl transition-all duration-500 bg-gradient-to-br to-hotpink via-myTeal from-lightgreen bg-size-200 bg-pos-0 w-full lg:w-auto h-12 flex justify-center items-center hover:bg-pos-100 hover:text-white'type='button' >
+            <button className='text-navy text-lg uppercase rounded-2xl border-2 border-myTeal w-full lg:w-auto h-12 flex justify-center items-center hover:bg-pos-100 hover:text-white'type='button' >
               Twitter
               </button>
            </a>
            <a href='https://github.com/lellis09'target='_blank' rel='noreferrer'>
-            <button className='text-navy text-lg uppercase rounded-2xl transition-all duration-500 bg-gradient-to-br to-hotpink via-myTeal from-lightgreen bg-size-200 bg-pos-0 w-full lg:w-auto h-12 flex justify-center items-center hover:bg-pos-100 hover:text-white'type='button' >
+            <button className='text-navy text-lg uppercase rounded-2xl border-2 border-myTeal w-full lg:w-auto h-12 flex justify-center items-center hover:bg-pos-100 hover:text-white'type='button' >
               Github
               </button>
            </a>
@@ -38,12 +38,12 @@ export default function Home() {
               </button>
            </a> */}
           <a href='https://www.linkedin.com/in/lishia-ellis/'target='_blank' rel='noreferrer'>
-            <button className='text-navy text-lg uppercase rounded-2xl transition-all duration-500 bg-gradient-to-br to-hotpink via-myTeal from-lightgreen bg-size-200 bg-pos-0 w-full lg:w-auto h-12 flex justify-center items-center hover:bg-pos-100 hover:text-white'type='button' >
+            <button className='text-navy text-lg uppercase rounded-2xl border-2 border-myTeal w-full lg:w-auto h-12 flex justify-center items-center hover:bg-pos-100 hover:text-white'type='button' >
               Linkedin
               </button>
            </a>
            <a href='https://hashnode.com/@tealternary'target='_blank' rel='noreferrer'>
-            <button className='text-navy text-lg uppercase rounded-2xl transition-all duration-500 bg-gradient-to-br to-hotpink via-myTeal from-lightgreen bg-size-200 bg-pos-0 w-full lg:w-auto h-12 flex justify-center items-center hover:bg-pos-100 hover:text-white'type='button' >
+            <button className='text-navy text-lg uppercase rounded-2xl border-2 border-myTeal w-full lg:w-auto h-12 flex justify-center items-center hover:bg-pos-100 hover:text-white'type='button' >
               Hashnode
               </button>
            </a>
